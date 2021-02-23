4bf9512370803b3ae1f55d23a285945ef22ee994
Alexandra Rodriguez-Beuerman <arbeuerman@gmail.com>
2021-02-23 23:08:41 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -22,13 +22,23 @@ def literal_aoh
   # Using Array literal syntax only, build a nested array that uses the data in
   # held in the constants
   people = [
-    DON_G, 
-    JOELLE_VD, 
-    PAT_M, 
-    KATE_G, 
-    BRUCE_G
-    ]
-  people.sort
+    { 
+      name:  "Joelle van Dyne", 
+      occupation: "Radio Personality"
+    },
+    { 
+      name:  "Pat Monteseian", 
+      occupation: "Staff"
+    },
+    { 
+      name:  "Kate Gompert", 
+      occupation: "None"
+    }, 
+    { 
+      name:  "Bruce Green", 
+      occupation: "Fan of Mildred"
+    }
+  ]
 end
 
 def aoh_lookup(aoh, row, key)
